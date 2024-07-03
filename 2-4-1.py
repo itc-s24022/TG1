632b01128fe544c06799b6ad308b4cfb79536ae5
itc-s24022 <s24022@std.it-college.ac.jp>
2024-07-03 08:00:24 +0000

    commit

---
+++ 2-4-1.py
@@ -0,0 +1,12 @@
+print(type(3))
+print(type(3.1))
+a = 3
+print(type(a))
+print(type(a)is int)
+print(type(a)is float)
+print(isinstance(a, int))
+
+
+
+
+
---
+++ 2-5.py
@@ -0,0 +1,17 @@
+print('aaa')
+print("aaa")
+print(type('aaa'))
+
+aaa = '''aaaaaaaaaa
+aaaaaaaaaa
+aaaaaaaaaa'''
+
+print(aaa)
+
+my_str = 'この後改行\nしてたらタブ\tで空白'
+print(my_str)
+
+print('a') #comment
+
+
+
