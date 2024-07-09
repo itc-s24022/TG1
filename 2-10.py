a8c7c7abd9d4cc5f9eff54799d6b0a955fd8f564
itc-s24022 <167942819+itc-s24022@users.noreply.github.com>
2024-07-09 16:39:05 +0900

    ch2

---
+++ 2-10.py
@@ -0,0 +1,17 @@
+pow(2, 3)
+pow(3, 2)
+print(2, 3, 4)
+print(4, 3, 2)
+
+print(2, 3, 4, sep="/")
+print(2, 3, 4, sep="-")
+
+print(2, 3, 4, sep="/", end='---')
+print(2, 3, 4, sep="/", end='---')
+
+help(print)
+
+arg_vals = [2,3,4]
+print(*arg_vals)
+
+print(arg_vals)
